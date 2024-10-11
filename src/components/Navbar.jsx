692c31ac15dd0c7d407d6f6d1b6b30572ea5859c
Md. Jillur Rahman <105236203+jillur984@users.noreply.github.com>
2024-10-11 09:10:30 +0600

    navbar design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,32 @@
-
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-   <>
-   <div className="h-12 bg-green-600 flex items-center">
+    <>
+      <nav className="h-16 bg-green-500 flex justify-end items-center shadow-md">
+        <ul className="flex gap-6 m-4 font-semibold text-black">
+          <li className="hover:bg-green-700 p-2 rounded-md transition duration-300">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="hover:bg-green-700 p-2 rounded-md transition duration-300">
+            <Link to="/doctor">Doctor</Link>
+          </li>
+          <li className="hover:bg-green-700 p-2 rounded-md transition duration-300">
+            <Link to="/patient">Patient</Link>
+          </li>
+          <li className="hover:bg-green-700 p-2 rounded-md transition duration-300">
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li className="hover:bg-green-700 p-2 rounded-md transition duration-300">
+            <Link to="/report">Report</Link>
+          </li>
+          <li className="hover:bg-green-700 p-2 rounded-md transition duration-300">
+            <Link to="/intern">Internship Care</Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
 
-   </div>
-   </>
-  )
-}
-
-export default Navbar
+export default Navbar;
